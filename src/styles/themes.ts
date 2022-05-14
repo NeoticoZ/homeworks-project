c82ff04a6956e63d1ae449ebd62fa57a39e1a80e
NeoticoZ <heriksaraujodias@gmail.com>
2022-05-13 21:14:51 -0300

    feat: dark mode

---
+++ src/styles/themes.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  primary: "#fff",
+  secondary: "#f1f3f6",
+  text: "#4c4d4d",
+};
+
+export const darkTheme = {
+  primary: "#212529",
+  secondary: "#343a40",
+  text: "#fff",
+};
